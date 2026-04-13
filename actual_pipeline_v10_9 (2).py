cce8aa639ae45c8fdaf2c34e02ff0181948a180e
dnzsls <beyzasls123@gmail.com>
2026-04-13 15:29:38 +0300

    Add cross_queue configuration to pipeline

--- actual_pipeline_v10_9 (2).py
+++ actual_pipeline_v10_9 (2).py
@@ -6,3 +6,10 @@
         {'name': 'aksam', 'start': '12:00', 'end': '23:30', 'penalty': 2.0},
     ]
 },
+
+'cross_queue': {
+    'enabled': True,
+    'donors': ['gold', 'kurumsal'],
+    'receiver': 'kitle',
+    'transfer_ratio': 1.0,
+},
