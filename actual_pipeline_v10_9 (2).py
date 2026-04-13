99c10c60b4cdc6943d3b9ed0a3cf7836682e23df
dnzsls <beyzasls123@gmail.com>
2026-04-13 11:19:30 +0300

    Update actual_pipeline_v10_9 (2).py

--- actual_pipeline_v10_9 (2).py
+++ actual_pipeline_v10_9 (2).py
@@ -3,3 +3,8 @@
     'donors': ['gold', 'kurumsal'],   # fazlalığını kitleye veren kuyruklar
     'receiver': 'kitle',               # overflow alan kuyruk
 },
+
+
+from actual_pipeline_v3_weekday import *
+
+results = run_all_queues(df_calls, df_actual, df_shifts_dict, '2025-02-17', config=CONFIG)
