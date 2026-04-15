e77396c34f7083e28754ed33c8e2ee9862863cfd
dnzsls <beyzasls123@gmail.com>
2026-04-15 14:45:06 +0300

    Update actual_pipeline_v10_9 (2).py

--- actual_pipeline_v10_9 (2).py
+++ actual_pipeline_v10_9 (2).py
@@ -1,6 +1,2 @@
 for q in ['kitle', 'kurumsal', 'gold']:
-    mps = CONFIG['queue_configs'][q]['mip']['min_per_shift']
-    asg = results[q]['mip_info']['assignments']
-    kucuk = {s: c for s, c in asg.items() if 0 < c < mps}
-    if kucuk:
-        print(f"{q} (min={mps}): {kucuk}")
+    print(q, "min_per_shift:", CONFIG['queue_configs'][q]['mip']['min_per_shift'])
