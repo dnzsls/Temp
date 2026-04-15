62049c8dac13e583b4f685f7cf79062744cf0c87
dnzsls <beyzasls123@gmail.com>
2026-04-15 14:30:13 +0300

    Update actual_pipeline_v10_9 (2).py

--- actual_pipeline_v10_9 (2).py
+++ actual_pipeline_v10_9 (2).py
@@ -1,5 +1,7 @@
-print("min_per_shift:", CONFIG['queue_configs']['kitle']['mip']['min_per_shift'])
-result = run_queue_pipeline(df_calls, df_actual, df_shifts, '2026-02-17', 'kitle', config=CONFIG)
-print("V04_outsource:", result['mip_info']['assignments'].get('V04_outsource'))
+results = run_all_queues(df_calls, df_actual, df_shifts, '2026-02-17', config=CONFIG)
 
-df_shifts['kitle'][df_shifts['kitle']['start'] == '08:00']
+print("min_per_shift:", CONFIG['queue_configs']['kitle']['mip']['min_per_shift'])
+print("V04_outsource:", results['kitle']['mip_info']['assignments'].get('V04_outsource'))
+
+# Catalog'da V04 kaç satır?
+print(df_shifts['kitle'][df_shifts['kitle']['start'] == '08:00'])
