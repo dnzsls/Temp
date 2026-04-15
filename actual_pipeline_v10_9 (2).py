8800a92bb7097ec5674fe7c75f8377e30d5cc635
dnzsls <beyzasls123@gmail.com>
2026-04-15 14:42:10 +0300

    Update actual_pipeline_v10_9 (2).py

--- actual_pipeline_v10_9 (2).py
+++ actual_pipeline_v10_9 (2).py
@@ -1,7 +1,6 @@
-results = run_all_queues(df_calls, df_actual, df_shifts, '2026-02-17', config=CONFIG)
-
-print("min_per_shift:", CONFIG['queue_configs']['kitle']['mip']['min_per_shift'])
-print("V04_outsource:", results['kitle']['mip_info']['assignments'].get('V04_outsource'))
-
-# Catalog'da V04 kaç satır?
-print(df_shifts['kitle'][df_shifts['kitle']['start'] == '08:00'])
+for q in ['kitle', 'kurumsal', 'gold']:
+    mps = CONFIG['queue_configs'][q]['mip']['min_per_shift']
+    asg = results[q]['mip_info']['assignments']
+    kucuk = {s: c for s, c in asg.items() if 0 < c < mps}
+    if kucuk:
+        print(f"{q} (min={mps}): {kucuk}")
