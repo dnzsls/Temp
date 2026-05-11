8f05d38a6dcd6fb9749b93999c2a8f95da8907d3
dnzsls <beyzasls123@gmail.com>
2026-05-11 17:38:19 +0300

    Update actual_pipeline_v5_weekday.py

--- actual_pipeline_v5_weekday.py
+++ actual_pipeline_v5_weekday.py
@@ -1,9 +1,1 @@
-'start_smoothing': {
-    'enabled': True,
-    'companies': ['inhouse', 'outsource'],
-    'windows': [
-        {'name': 'sabah',     'start': '07:00', 'end': '10:00', 'penalty': 3.0},
-        {'name': 'ogle_peak', 'start': '10:00', 'end': '15:00', 'penalty': 15.0},
-        {'name': 'aksam',     'start': '15:00', 'end': '20:00', 'penalty': 6.0},
-    ],
-},
+print(CONFIG_WEEKDAY['queue_configs']['kitle']['slot_cap'])
