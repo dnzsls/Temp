2cb2849b87eb68711d7612f1491806ab714481c7
dnzsls <beyzasls123@gmail.com>
2026-05-11 17:38:43 +0300

    Update actual_pipeline_v5_weekday.py

--- actual_pipeline_v5_weekday.py
+++ actual_pipeline_v5_weekday.py
@@ -1,1 +1,5 @@
 print(CONFIG_WEEKDAY['queue_configs']['kitle']['slot_cap'])
+mi = results['2026-02-09']['kitle']['mip_info']
+print("slot_cap_detail satır:", len(mi.get('slot_cap_detail', [])))
+print("sc_excess_by_slot satır:", len(mi.get('sc_excess_by_slot', {})))
+print(mi.get('slot_cap_detail', [])[:3])
